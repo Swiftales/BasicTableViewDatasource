4f42d5272af6747901daea52537e6da82cea949f
Vishal Singh Panwar <vishal.panwar@mutualmobile.com>
2016-12-11 10:58:48 +0530

    Initial Commit

---
+++ BasicTableViewDatasource/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BasicTableViewDatasource
+//
+//  Created by Vishal Singh on 12/11/16.
+//  Copyright © 2016 Vishal Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
